4f57b5cfc0865474d333fa8143aa8bf6dd1179ed
Lawal Hayde <81729609+Haydelawal@users.noreply.github.com>
2023-02-13 08:01:03 +0100

    Delete android-basics-kotlin-bus-schedule-app-starter directory

--- android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/data/BusScheduleApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.example.busschedule.data
-
-import android.app.Application
-
-class BusScheduleApplication : Application() {
-    val database: AppDatabase by lazy { AppDatabase.getDatabase(this) }
-}
